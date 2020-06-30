deaa73f4c8447bd0be960cade3bb733643a88f3f
Naveen <thenvnsahu@gmail.com>
2020-06-30 19:56:37 +0530

    set up permission requests

--- app/src/main/java/in/thenvn/artista/MainActivity.kt
+++ app/src/main/java/in/thenvn/artista/MainActivity.kt
@@ -1,11 +1,67 @@
 package `in`.thenvn.artista
 
+import android.Manifest
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
+import pub.devrel.easypermissions.PermissionRequest
 
-class MainActivity : AppCompatActivity() {
+private const val RC_PERMISSIONS: Int = 2
+
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
+    private val TAG: String = MainActivity::class.java.name
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestPermissions()
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    @AfterPermissionGranted(RC_PERMISSIONS)
+    private fun requestPermissions() {
+        val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        if (EasyPermissions.hasPermissions(this, *permissions)) {
+            Log.d(TAG, "Permissions already granted.")
+        } else {
+            EasyPermissions
+                .requestPermissions(PermissionRequest.Builder(this, RC_PERMISSIONS, *permissions)
+                    .setRationale("Required permissions")
+                    .setPositiveButtonText("GRANT")
+                    .setNegativeButtonText("DENY")
+                    .build())
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        Log.d(TAG, "Permission is granted")
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE) {
+            Log.d(TAG, "Is permission grated?")
+        }
+    }
+
 }
