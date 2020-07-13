def72b56b75f652ceb038989f593bc864eb19f68
Naveen <thenvnsahu@gmail.com>
2020-07-13 19:19:48 +0530

    added binding attr for Style objects

--- app/src/main/java/in/thenvn/artista/utils/BindingUtils.kt
+++ app/src/main/java/in/thenvn/artista/utils/BindingUtils.kt
@@ -1,5 +1,6 @@
 package `in`.thenvn.artista.utils
 
+import `in`.thenvn.artista.editor.Style
 import `in`.thenvn.artista.media.MediaItem
 import android.net.Uri
 import android.widget.ImageView
@@ -19,5 +20,14 @@ fun ImageView.setSrcMedia(item: MediaItem) {
 fun ImageView.setSrcUri(uri: Uri) {
     Glide.with(context)
         .load(uri)
+        .into(this)
+}
+
+@BindingAdapter("srcStyle")
+fun ImageView.setSrcStyle(style: Style) {
+    Glide.with(context)
+        .load(style.uri)
+        .override(300)
+        .centerInside()
         .into(this)
 }
