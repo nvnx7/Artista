93e19580889cb06c075bbfa7aefe46aacc6a16fb
Naveen <thenvnsahu@gmail.com>
2020-07-20 00:30:02 +0530

    added type property & valid values

--- app/src/main/java/in/thenvn/artista/editor/Style.kt
+++ app/src/main/java/in/thenvn/artista/editor/Style.kt
@@ -2,5 +2,9 @@ package `in`.thenvn.artista.editor
 
 import android.net.Uri
 
-data class Style(val uri: Uri) {
+data class Style(val uri: Uri, val type: Int) {
+    companion object {
+        const val FIXED = 0
+        const val CUSTOM = 1
+    }
 }
