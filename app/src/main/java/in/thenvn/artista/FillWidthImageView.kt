f574bbb47b883e8121b6a37dd32a0307ec46abdc
Naveen <thenvnsahu@gmail.com>
2020-07-22 19:04:29 +0530

    custom image view to hold preview of edit

---
+++ app/src/main/java/in/thenvn/artista/FillWidthImageView.kt
@@ -0,0 +1,26 @@
+package `in`.thenvn.artista
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+/**
+ * Custom image view that makes the drawable width to be same as the view regardless of
+ * height of drawable, while also maintaining aspect ratio of drawable.
+ */
+class FillWidthImageView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatImageView(context, attrs, defStyleAttr) {
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        if (drawable != null) {
+            val w: Int = MeasureSpec.getSize(widthMeasureSpec)
+            val h: Int = w * drawable.intrinsicHeight / drawable.intrinsicWidth
+            setMeasuredDimension(w, h)
+        } else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        }
+    }
+}
