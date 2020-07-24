e7e94fd1849552cefc6fc70f8f2f420b7a9beecf
Naveen <thenvnsahu@gmail.com>
2020-07-24 20:44:48 +0530

    added property to show selection in UI

--- app/src/main/java/in/thenvn/artista/editor/Style.kt
+++ app/src/main/java/in/thenvn/artista/editor/Style.kt
@@ -2,9 +2,14 @@ package `in`.thenvn.artista.editor
 
 import android.net.Uri
 
-data class Style(val uri: Uri, val type: Int) {
+data class Style(
+    val uri: Uri,
+    val type: Int,
+    var selected: Boolean = false
+) {
     companion object {
         const val FIXED = 0
         const val CUSTOM = 1
     }
+
 }
