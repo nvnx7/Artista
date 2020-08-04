93add58c5de82a59a5f338a974929be917c3b265
Naveen <thenvnsahu@gmail.com>
2020-08-04 17:10:07 +0530

    added menu to show activity with oss license notices

--- app/src/main/java/in/thenvn/artista/MainActivity.kt
+++ app/src/main/java/in/thenvn/artista/MainActivity.kt
@@ -1,7 +1,12 @@
 package `in`.thenvn.artista
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +17,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        OssLicensesMenuActivity.setActivityTitle(getString(R.string.title_licenses_activity))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.about -> {
+                startActivity(Intent(this, OssLicensesMenuActivity::class.java))
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
